2e58e82476cf20dc57f129bf145f8294bf8da19b
JontyWang <501211312@qq.com>
2020-04-03 10:15:15 +0800

    更改视图

---
+++ Migrations/20200403005045_move.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StudentMvc.Migrations
+{
+    public partial class move : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
